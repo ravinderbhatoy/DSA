931313a64a70272573c9b4ca72a1274fff029ae8
ravinderbhatoy <ravinderd.bhatoy@gmail.com>
2026-06-01 18:02:20 +0530

    alot of questions

--- python/linkedlists/merge2Sorted.py
+++ python/linkedlists/merge2Sorted.py
@@ -1,4 +1,5 @@
 from linkedlist import Llist
+from linkedlist import Node
 
 
 def mergeLlist(h1, h2):
@@ -13,6 +14,26 @@ def mergeLlist(h1, h2):
         return h2
 
 
+def merge2Lists(h1, h2):
+    dummy = Node(-1)
+    mover = dummy
+    while h1 and h2:
+        if h1.data <= h2.data:
+            mover.next = h1
+            h1 = h1.next
+        else:
+            mover.next = h2
+            h2 = h2.next
+        mover = mover.next
+
+    if h1:
+        mover.next = h1
+    elif h2:
+        mover.next = h2
+
+    return dummy.next
+
+
 l1 = Llist(data=[1, 3, 5])
 l2 = Llist(data=[1, 2, 6])
 
@@ -22,6 +43,7 @@ l2.show()
 h1 = l1.get_head()
 h2 = l2.get_head()
 
-mergeLlist(h1, h2)
+# mergeLlist(h1, h2)
+merge2Lists(h1, h2)
 
 l1.show()
